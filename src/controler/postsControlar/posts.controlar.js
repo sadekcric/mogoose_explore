e95a35e4472d60967b73868ec99878388b095f8a
sadekcric4 <sadekcric4@gmail.com>
2024-07-14 13:15:50 +0600

    post create, get, getById implementation

--- src/controler/postsControlar/posts.controlar.js
+++ src/controler/postsControlar/posts.controlar.js
@@ -1,5 +1,36 @@
-const postsGet = (req, res) => {
-  res.status(200).json({ message: "Posts Get Connected" });
+const Posts = require("../../models/posts");
+
+// Get Posts
+const postsGet = async (req, res) => {
+  try {
+    const allPosts = await Posts.find();
+
+    res.status(200).json(allPosts);
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
 };
 
-module.exports = { postsGet };
+// createPosts
+const createPosts = async (req, res) => {
+  try {
+    const posts = await Posts.create(req.body);
+
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
+};
+
+// Get post By Id
+const getPostById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getPost = await Posts.findById(id).populate("author");
+    res.status(200).json(getPost);
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
+};
+
+module.exports = { postsGet, createPosts, getPostById };
--- src/routes/posts/posts.route.js
+++ src/routes/posts/posts.route.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { postsGet } = require("../../controler/postsControlar/posts.controlar");
+const { postsGet, createPosts, getPostById } = require("../../controler/postsControlar/posts.controlar");
 
 const route = express.Router();
 
 route.get("/", postsGet);
+route.post("/", createPosts);
+route.get("/:id", getPostById);
 
 module.exports = route;
